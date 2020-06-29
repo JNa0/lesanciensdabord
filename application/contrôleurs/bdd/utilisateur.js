52bfd71d5dbcc3fbd654c1ebd6b75ffa0d2e6e98
Florian Monfort <florian.monfort@etudiant.univ-rennes1.fr>
2020-06-29 11:35:29 +0200

    -

--- application/contrôleurs/bdd/utilisateur.js
+++ application/contrôleurs/bdd/utilisateur.js
@@ -2,28 +2,55 @@
 const clientMongo = (function () {
 	let accèsBDD
 
-	require("mongodb").MongoClient.connect("lesanciensdabord", (erreur, client) => {
+	require("mongodb").MongoClient.connect("lesanciensdabord")
+	.then(client => {
+		accèsBDD = client
+	})
+	.catch(erreur => {
 		if (erreur)
 			throw erreur
-
-		else {
-			accèsBDD = client
-		}
 	})
 
-	return accèsBDD
+	return accèsBDD.db("lesanciensdabord") || null
 })
 
 module.exports = {
 	"creer": function (requête, réponse) {
-		réponse.render("tout va bien")
+		const membresBDD = accèsBDD.collection("membres")
+
+		Object.keys(requête.params).forEach(([clé, valeur]) => {
+			réponse.write(`${clé} : ${valeur}\n`)
+		}
+
+		//requête.write(requête.body)
+		/*
+		membres.insertOne({
+			"nom": requête.params.nom,
+			"prénom": requête.params.prénom,
+			"dateNaissance": requête.params.dateNaissance,
+		})
+		.then(() => {
+			réponse.write("\nSuccès d’insertion\n")
+		})
+		.catch(erreur => {
+			throw erreur
+		})
+		*/
+
+		réponse.send("\ntout va bien")
 	},
 
 	"modifier": function () {
-		réponse.render("tout va bien")
+		Object.keys(requête.params).forEach(([clé, valeur]) => {
+			réponse.write(`${clé} : ${valeur}\n`)
+		}
+		réponse.send("\ntout va bien")
 	},
 
 	"supprimer": function () {
-		réponse.render("tout va bien")
+		Object.keys(requête.params).forEach(([clé, valeur]) => {
+			réponse.write(`${clé} : ${valeur}\n`)
+		}
+		réponse.send("\ntout va bien")
 	},
 }
